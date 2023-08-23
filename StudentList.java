4e466c091549140370b9fc15cc7251d82e96d852
Raj <rahathossain1510508260@gmail.com>
2023-08-24 02:07:57 +0600

    Step-0

--- StudentList.java
+++ StudentList.java
@@ -13,8 +13,13 @@ public class StudentList {
 							new FileInputStream("students.txt"))); 
 			String r = s.readLine();
 			String i[] = r.split(",");			
-			for(String j : i) { System.out.println(j); }
-			} catch (Exception e){} 
+			for(String j : i) {
+				System.out.println(j);
+			}
+			s.close();
+			}
+			catch (Exception e){
+			}
 			System.out.println("Data Loaded.");
 		}
 		else if(args[0].equals("r")) 
@@ -25,12 +30,15 @@ public class StudentList {
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
 			String r = s.readLine();
-			System.out.println(r);
+			//System.out.println(r);
 			String i[] = r.split(",");	
 			Random x = new Random();
-				int y = x.nextInt();
+				int y = x.nextInt(i.length);
 					System.out.println(i[y]);
-			} catch (Exception e){} 
+			s.close();
+			}
+			catch (Exception e){
+			}
 			System.out.println("Data Loaded.");			
 		}
 		else if(args[0].contains("+")){
@@ -45,7 +53,9 @@ public class StudentList {
 	        String fd= dateFormat.format(d);
 			s.write(", "+t+"\nList last updated on "+fd);
 			s.close();
-			} catch (Exception e){}
+			}
+			catch (Exception e){
+			}
 							
 			System.out.println("Data Loaded.");	
 		}
@@ -57,16 +67,31 @@ public class StudentList {
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
 			String r = s.readLine();
-			String i[] = r.split(",");	
-			boolean done = false;
+			String i[] = r.split(", ");
+			String t = args[0].substring(1);
+			int k=0;
+			for(int j=0;j<i.length;j++){
+				if(t.equals(i[j])){
+					System.out.println("We found it!");
+					k=1;
+					break;
+				}
+			}
+			if(k!=1){
+				System.out.println("We not found");
+			}
+			/*boolean done = false;
 			String t = args[0].substring(1);
 			for(int idx = 0; idx<i.length && !done; idx++) {
 				if(i[idx].equals(t)) {
 					System.out.println("We found it!");
 						done=true;
 				}
+			}*/
+			s.close();
 			}
-			} catch (Exception e){} 
+			catch (Exception e){
+			}
 			System.out.println("Data Loaded.");				
 		}
 		else if(args[0].contains("c")) 
@@ -77,7 +102,8 @@ public class StudentList {
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
 			String D = s.readLine();
-			char a[] = D.toCharArray();			
+			String i[] = D.split(",");
+			/*char a[] = D.toCharArray();
 			boolean in_word = false;
 			int count=0;
 			for(char c:a) {
@@ -86,10 +112,16 @@ public class StudentList {
 					if (!in_word) {	count++; in_word =true;	}
 					else { in_word=false;}			
 				}
+			}*/
+			System.out.println(i.length +" word(s) found ");
+			s.close();
 			}
-			System.out.println(count +" word(s) found " + a.length);
-			} catch (Exception e){} 
+			catch (Exception e){
+			}
 			System.out.println("Data Loaded.");				
+		}
+		else{
+			System.out.println("We found it!");
 		}
 	}
 }
